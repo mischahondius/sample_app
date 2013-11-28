ee38bffb983f7d2d82cf3cdda7629c1c7d4af513
Mischa Hondius <mhondius@gmail.com>
2013-11-28 17:50:47 +0100

    Add user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,30 @@ class UsersController < ApplicationController
   
   def show
   @user = User.find(params[:id])
+  @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
-  @user = User.new
+    if signed_in?
+       redirect_to root_url
+
+    else
+      @user = User.new
+    end  
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"	
-      redirect_to @user
-    else
-      render 'new'
+    if signed_in?
+       redirect_to root_url
+     else
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"	
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 
@@ -39,9 +49,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
+    @user = User.find(params[:id])
+    if current_user?(@user)
+      redirect_to users_path, notice: "You can't destroy yourself."
+
+    else  
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    end
   end
 
 
@@ -54,13 +70,13 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
     end
-
+  end
+  
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
@@ -69,4 +85,6 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,10 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+    it { should_not have_link('Users') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+    it { should_not have_link('Sign out',    href: signout_path) }
   end
 
   describe "signin" do
@@ -19,15 +23,16 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should_not have_link('Users') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out',    href: signout_path) }
+
     end
 
   	describe "with valid information" do
   	      let(:user) { FactoryGirl.create(:user) }
-  	      before do
-  	        fill_in "Email",    with: user.email.upcase
-  	        fill_in "Password", with: user.password
-  	        click_button "Sign in"
-  	      end
+          before { sign_in user }
 
   	      it { should have_title(user.name) }
           it { should have_link('Users',       href: users_path) }
@@ -45,6 +50,17 @@ describe "Authentication" do
 
   describe "authorization" do
 
+      describe "as admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:admin) { FactoryGirl.create(:admin) }
+
+      before { sign_in admin, no_capybara: true }
+
+        describe "should not be able to delete themselves" do
+          it { expect { delete user_path(admin) }.not_to change(User, :count) }
+        end
+    end
+
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
@@ -60,6 +76,52 @@ describe "Authentication" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end
+        end
+      end
+
 
       describe "when attempting to visit a protected page" do
         before do
